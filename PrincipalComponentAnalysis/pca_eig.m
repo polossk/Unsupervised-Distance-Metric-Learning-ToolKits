d05cd9dee8031ba4fc2ffd335a85faa691d1cecc
Shen <polossk@126.com>
2016-05-09 15:27:17 +0800

    Bug Fix!
    Fixed the bug ablout issue #1 , by modifying
    PrincipalComponentAnalysis\pca_eig.m and
    PrincipalComponentAnalysis\pca_svd.m

--- PrincipalComponentAnalysis/pca_eig.m
+++ PrincipalComponentAnalysis/pca_eig.m
@@ -1,19 +1,55 @@
 %% Principal Component Analysis (PCA), Using Eigenvalue Decomposition.
-% |function[D, W, mu] = pca_eig(X, n)|
+% |function[D, W, mu] = pca_eig(X, n, flag)|
 %
-%% *Input*
+% * Author:   Shangkun.Shen
+% * Method:   Eigenvalue Decomposition
+%
+%% *Usage*
+%
+% * |[D, W, mu] = pca_eig(X, n);|
+%
+% * |[D, W, mu] = pca_eig(X, n, '-all');|
+%
+% * |[D, W, mu] = pca_eig(X, n, '-single');|
+%
+%% *Input Arguments*
 % * |X|: N by P data matrix. Rows of X correspond to observations
 % and columns to variables.
 % * |n|: number of principal component.
+% * |flag|: pca option, '-single' by default, setting |mu| to the average
+% of each columns, also could be '-all', setting |mu| to the average of the
+% whole data.
 %
-%% *Output*
+%% *Output Results*
 % * |D|: the principal component variances
 % * |W|: the principal component coefficients
-% * |mu|: average of X
+% * |mu|: average of X, determined by |flag|
 %
-%% *Source*
-function [D, W, mu] = pca_eig(X, n)
-    mu = mean(X, 1);
+%% *Source Code*
+function [D, W, mu] = pca_eig( varargin )
+    if (nargin < 2)
+        warning 'Lack of input arguments. Terminates.'; return;
+    elseif (nargin > 3)
+        warning 'Too many input arguments. Terminates.'; return;
+    elseif (isnumeric(varargin{1}) && isnumeric(varargin{2}))
+        X = varargin{1}; n = varargin{2};
+        if (nargin == 3 && ischar(varargin{3}))
+            flag = varargin{3};
+        else
+            warning 'Wrong input arguments. Terminates.'; return;
+        end
+    else 
+        warning 'Wrong input arguments. Terminates.'; return;
+    end
+
+    if strcmp('-all', flag)
+        mu = mean(X(:));
+    elseif strcmp('-single', flag)
+        mu = mean(X, 1);
+    else
+        warning 'Wrong flag setting. Terminates.'; return;
+    end
+
     X = bsxfun(@minus, X, mu);
     [W, D] = eigs(X * X', n);
 end
--- PrincipalComponentAnalysis/pca_svd.m
+++ PrincipalComponentAnalysis/pca_svd.m
@@ -1,31 +1,64 @@
 %% Principal Component Analysis (PCA), Using Singular Value Decomposition.
 % |function[D, W, mu] = pca_svd(X, n)|
 %
-%% *Input*
+% * Author:   Shangkun.Shen
+% * Method:   Singular Value Decomposition
+%
+%% *Usage*
+%
+% * |[D, W, mu] = pca_svd(X, n);|
+%
+% * |[D, W, mu] = pca_svd(X, n, '-all');|
+%
+% * |[D, W, mu] = pca_svd(X, n, '-single');|
+%
+%% *Input Arguments*
 % * |X|: N by P data matrix. Rows of X correspond to observations
 % and columns to variables.
 % * |n|: number of principal component.
+% * |flag|: pca option, '-single' by default, setting |mu| to the average
+% of each columns, also could be '-all', setting |mu| to the average of the
+% whole data.
 %
-%% *Output*
+%% *Output Results*
 % * |D|: the principal component variances
 % * |W|: the principal component coefficients
-% * |mu|: average of X
+% * |mu|: average of X, determined by |flag|
 %
-%% *Source*
-function [D, W, mu] = pca_svd(X, n)
-    mu = mean(X, 1);
+%% *Source Code*
+function [D, W, mu] = pca_svd( varargin )
+    if (nargin < 2)
+        warning 'Lack of input arguments. Terminates.'; return;
+    elseif (nargin > 3)
+        warning 'Too many input arguments. Terminates.'; return;
+    elseif (isnumeric(varargin{1}) && isnumeric(varargin{2}))
+        X = varargin{1}; n = varargin{2};
+        if (nargin == 3 && ischar(varargin{3}))
+            flag = varargin{3};
+        else
+            warning 'Wrong input arguments. Terminates.'; return;
+        end
+    else 
+        warning 'Wrong input arguments. Terminates.'; return;
+    end
+
+    if strcmp('-all', flag)
+        mu = mean(X(:));
+    elseif strcmp('-single', flag)
+        mu = mean(X, 1);
+    else
+        warning 'Wrong flag setting. Terminates.'; return;
+    end
+
     [w, ~] = size(X);
     X = bsxfun(@minus, X, mu);
     [u, s, ~] = svd(X, 0);
     [~, l] = size(s);
-    D = zeros(n, n);
-    W = zeros(w, n);
+    D = zeros(n, n); W = zeros(w, n);
     if l < n
-        D(1 : l, 1 : l) = s;
-        W(  :  , 1 : l) = u;
+        D(1 : l, 1 : l) = s; W(  :  , 1 : l) = u;
     else
-        D = s(1 : n, 1 : n);
-        W = u(  :  , 1 : n);
+        D = s(1 : n, 1 : n); W = u(  :  , 1 : n);
     end
 end
 %% *Note*
