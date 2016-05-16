8e56fc4b1afa8abf41051040428c61fb4db7ee17
Shen <polossk@126.com>
2016-05-16 21:58:25 +0800

    update mds.m

--- MultipleDimensionalScaling/mds.m
+++ MultipleDimensionalScaling/mds.m
@@ -31,46 +31,52 @@ function [ Y, e ] = mds( D, p )
     if (nargin < 1)
         warning 'Lack of input arguments. Terminates.'; return;
     elseif (nargin > 2)
-    	warning 'Too many input arguments. Terminates.'; return;
+        warning 'Too many input arguments. Terminates.'; return;
     end
     
-    [n, m] = size(D);
-    
-    if (nargin == 2 && isscalar(p))
-    	p = min(p, n);
-    elseif (nargin == 1)
-    	p = n;
-    end
-    
+    [n,m] = size(D);
+    del = 10 * eps(class(D));
+
     if (n == 1)
-    	n = ceil(sqrt(2 * m));
-    	if (n * (n - 1) / 2 == m) && all(D >= 0)
-    		D = squareform(D);
-    	else
-    		warning 'Bad input argument D. Terminates.'; return;
-    	end
+        n = ceil(sqrt(2 * m));
+        if (n * (n - 1) / 2 == m) && all(D >= 0)
+            D = squareform(D);
+        else
+            warning 'Bad input argument D. Terminates.'; return;
+        end
     elseif (n ~= m)
-    	warning 'Bad input argument D. Terminates.'; return;
+        warning 'Bad input argument D. Terminates.'; return;
     end
 
-    % P = eye(n) - repmat(1 / n, n, n);
+    if (nargin == 2 && isscalar(p))
+        p = min(p, n);
+    elseif (nargin == 1)
+        p = n;
+    end
+
+    % P = eye(n) - repmat(1/n,n,n);
     % B = P * (-.5 * D .* D) * P;
     % A more efficient way of doing the same thing.
-    D = D .* D; % square elements of D
+    D = D .* D;
     B = bsxfun( @plus, ...
             bsxfun( @minus, ...
                 bsxfun( @minus, D, sum(D, 1) / n ), ...
                 sum(D, 2) / n ), ...
             sum(D(:)) / (n ^ 2) ) * (-0.5);
-    [V, E] = eigs( (B + B') / 2, p ); e = diag(E);
-    keep = find(e > max(abs(e)) * eps(class(e)) ^ (3 / 4));
-
-    if isempty(keep)
-    	Y = zeros(n, 1);
+    if (p == n)
+        [V, E] = eig( (B + B') ./ 2);
     else
-    	Y = bsxfun(@times, V(:, keep), sqrt(e(keep))');
+        [V, E] = eigs( (B + B') ./ 2, p, 'LA');
     end
-    
+    [e, idx] = sort(diag(E), 'descend');
+
+    keep = find( e > max(abs(e)) * eps(class(e)) ^ (3 / 4) );
+    if isempty(keep)
+        Y = zeros(n, 1);
+    else
+        Y = bsxfun(@times, V(:, idx(keep)), sqrt(e(keep))');
+    end
+
     [~, maxind] = max(abs(Y), [], 1);
     d = size(Y, 2);
     colsign = sign(Y(maxind + (0 : n : (d - 1) * n)));
